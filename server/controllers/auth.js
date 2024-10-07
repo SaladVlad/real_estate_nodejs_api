0254dd6571a242d841fcb2e5c2635777ff0e8346
Vladislav Petković <vladislav.petkovic.02@gmail.com>
2024-10-07 10:06:32 +0200

    Implementing password reset API routes and controllers

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 import validator from "email-validator";
 import User from "../models/user.js";
-import { hashPassword } from "../helpers/auth.js";
+import { comparePassword, hashPassword } from "../helpers/auth.js";
 import {nanoid} from "nanoid";
 import jwt from "jsonwebtoken";
 
@@ -37,7 +37,6 @@ export const login = async (req,res) => {
                     process.env.JWT_SECRET,
                     {expiresIn: '7d'}
                 );
-                createdUser.password = undefined;
                 res.json({
                     token,
                     user: createdUser,
@@ -45,12 +44,55 @@ export const login = async (req,res) => {
             }
             catch(err){
                 return res.json({
-                    error: "Invalid email. Please use a valid email address"
+                    error: "Validation error"
+                });
+            }
+        }
+        else{
+            const match = await comparePassword(password,user.password);
+            if(!match){
+                return res.json({
+                    error: "Wrong password",
+                });
+            }
+            else{
+                const token = jwt.sign(
+                    {_id: createdUser._id},
+                    process.env.JWT_SECRET,
+                    {expiresIn: '7d'}
+                );
+
+                res.json({
+                    token,
+                    user,
                 });
             }
         }
     }
-    catch{
-
+    catch(err){
+        console.log("login error ",err);
+        res.json({
+            error: "Something went wrong. Try again."
+        });
     }
 }
+
+export const forgotPassword = async (req,res) => {
+    try{
+        const{email} = req.body;
+        let user = await User.findOne({email});
+        if(!user){
+            return res.json({error: "If we find your account, we will contact you shortly"});
+        }
+        else{
+            const password = nanoid(6);
+            //sending temporary password to the user's email using AWS services (can't right now)
+            user.password = hashPassword(password)
+            console.log(`Password reset initiated from ${email}, new password is:`,password)
+            await user.save();
+        }
+    }
+    catch (err) {
+
+    }
+};
--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -2,15 +2,18 @@ import { rejects } from "assert";
 import {randomBytes,pbkdf2} from "crypto"
 import { resolve } from "path";
 
-export const hashPassword = (password)=>{
-    const salt = randomBytes(16).toString("hex");
-    pbkdf2(password,salt, 1000,64,"sha512",(err,derivedKey)=>{
-        if(err){
+export const hashPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        const salt = randomBytes(16).toString("hex");
+        pbkdf2(password, salt, 1000, 64, "sha512", (err, derivedKey) => {
+        if (err) {
             reject(err);
         }
         resolve(`${salt}:${derivedKey.toString("hex")}`);
+        });
     });
-};
+   };
+   
 
 export const comparePassword = (password, hashed)=>{
     return new Promise((resolve,reject)=>{
